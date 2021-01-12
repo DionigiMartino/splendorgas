1357a689ff8dffac02aa556a9f065d3cd66e36c2
Dionigi Martino de Carles <40464068+DionigiMartino@users.noreply.github.com>
2021-01-12 13:56:53 +0100

    general

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,17 @@ function Header() {
             <div className={headerClasses.cont__header_cta}>
                 <img src={CallIcon} alt="Icon Call" /> 
                 <a href="tel:+390771674589">0771.67.45.89</a>
-            </div> 
+            </div>
+
+            <div className={headerClasses.cont__header_logo}>
+
+            </div>
+
+            <div className={headerClasses.cont__header_menuZone}>
+                <Link to="/contatti" className={headerClasses.cont__header_menuZone_cta}>
+                    Richiedi un Preventivo
+                </Link>
+            </div>
         </div>
     )
 }
