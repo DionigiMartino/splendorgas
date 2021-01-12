75aadc603686ce29fecf411137a41c26c9d00cb0
Dionigi Martino de Carles <40464068+DionigiMartino@users.noreply.github.com>
2021-01-12 12:39:14 +0100

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// import Style
+
+import headerClasses from '../../utilities/Sass/headerStyle.module.css'
+
+// importing Link to navigate 
+
+import {Link} from 'react-router-dom'
+
+function Header() {
+    return (
+        <div className={headerClasses.cont__header}>
+            
+        </div>
+    )
+}
+
+export default Header
