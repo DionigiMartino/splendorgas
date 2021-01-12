2ad67fe403eb3493080acabfe0282d4a22d8231e
Dionigi Martino de Carles <40464068+DionigiMartino@users.noreply.github.com>
2021-01-12 13:40:58 +0100

    trying

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CallIcon from '../../utilities/img/iconachiamatarossa.svg'
 
 // import Style
 
@@ -11,7 +12,10 @@ import {Link} from 'react-router-dom'
 function Header() {
     return (
         <div className={headerClasses.cont__header}>
-            
+            <div className={headerClasses.cont__header_cta}>
+                <img src={CallIcon} alt="Icon Call" /> 
+                <a href="tel:+390771674589">0771.67.45.89</a>
+            </div> 
         </div>
     )
 }
