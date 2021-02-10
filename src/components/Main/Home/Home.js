c666841a4970037effa2c86f4cea59e8964392d5
Dionigi Martino de Carles <40464068+DionigiMartino@users.noreply.github.com>
2021-02-10 23:28:43 +0100

    done

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -26,9 +26,7 @@ import Services from '../Services/Services'
 
 import Video from '../Video/Video';
 import video1 from '../../../utilities/img/video/video1.mp4'
-import video3 from '../../../utilities/img/video/video3.mp4'
 import screen1 from '../../../utilities/img/screen-video-1.jpg'
-import screen3 from '../../../utilities/img/screen-video-3.jpg'
 
 function Home() {
     return (
@@ -73,8 +71,6 @@ function Home() {
                 direttamente dai
                 <b> depositi costieri!</b>
             </p>
-
-            <Video videoSrc={video3} poster={screen3} />
 
             <div className={homeClasses.cont__home_descr}>
                 <h2>
